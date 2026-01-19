0f6352c9282f3e4b785441b83397f89e73145f54
Mahesa Bagus Raditya <mahesabagusraditya1@gmail.com>
2026-01-19 22:42:05 +0700

    Revert "Mahes"

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,25 +1,12 @@
-import Particles from '@/components/background/particles/Particles';
-import Jumbotron from './Components/Jumbotron';
-import Education from './Components/Education';
-
 export default function HomePage(): React.JSX.Element {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-4 text-center sm:px-6 lg:px-8">
-      <div className="pointer-events-auto fixed inset-0 z-0 h-full w-full">
-        <Particles
-          particleColors={['#1d175c', '#1d285c', '#1d5c8c']}
-          particleCount={500}
-          particleSpread={15}
-          speed={0.09}
-          particleBaseSize={150}
-          moveParticlesOnHover={true}
-          alphaParticles={false}
-          disableRotation={false}
-        />
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <div className="space-y-4">
+        <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+        <p className="text-lg text-muted-foreground">
+          This is the main page of our application.
+        </p>
       </div>
-
-      <Jumbotron />
-      <Education />
     </div>
   );
 }
