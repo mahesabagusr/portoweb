4dd6ca9d7ae43ac0faf6d804755e5d3f8208d0eb
Mahesa Bagus Raditya <mahesabagusraditya1@gmail.com>
2026-06-02 07:56:54 +0700

    feat: initialize project components, layout, and home page structure

--- src/components/custom/LenisProvider.tsx
+++ src/components/custom/LenisProvider.tsx
@@ -3,6 +3,12 @@
 import { useEffect } from 'react';
 import Lenis from 'lenis';
 
+declare global {
+  interface Window {
+    lenis?: Lenis;
+  }
+}
+
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
@@ -10,6 +16,8 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     });
+
+    window.lenis = lenis;
 
     function raf(time: number) {
       lenis.raf(time);
@@ -20,6 +28,7 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
 
     return () => {
       lenis.destroy();
+      window.lenis = undefined;
     };
   }, []);
 
